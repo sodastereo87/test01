68e9d1baa5c2c121f8ff652367585965c1a46302
sodastereo <f_er_@hotmail.com>
2023-12-01 20:13:53 -0500

    adding comments

--- src/components/FormCaptura.tsx
+++ src/components/FormCaptura.tsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 
+// describe la estructura de los datos que se van a capturar en el formulario
 interface FormData {
   nombre: string;
   edad: number;
   correo: string;
 }
 
+// describe las propiedades que el componente FormCaptura espera recibir con una única propiedad llamada onSubmit
 interface FormCapturaProps {
   onSubmit: (data: FormData) => void;
 }
 
+// toma las props definidas en FormCapturaProps
 const FormCaptura: React.FC<FormCapturaProps> = ({ onSubmit }) => {
+  // Utiliza el hook useState para manejar el estado local del formulario, formData es un objeto que contiene los valores indicados
   const [formData, setFormData] = useState<FormData>({
     nombre: '',
     edad: 0,
     correo: '',
   });
 
+  // función que se ejecuta cada vez que hay un cambio en los campos del formulario
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  // función que se activa cuando se envía el formulario y previene el comportamiento por defecto del formulario y llama a la función onSubmit
   const handleSubmit = (e: React.FormEvent): void => {
     e.preventDefault();
     onSubmit(formData);
