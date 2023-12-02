5fead8ac8785aac4f125db99ebc3d72e2a968146
sodastereo <f_er_@hotmail.com>
2023-12-01 20:18:01 -0500

    adding comments

--- src/components/FormPintar.tsx
+++ src/components/FormPintar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// describe las propiedades que el componente FormPintar espera recibir. Contiene una sola propiedad llamada data, que es un objeto con los siguentes campos nombre, edad y correo.
 interface FormPintarProps {
   data: {
     nombre: string;
@@ -8,6 +9,7 @@ interface FormPintarProps {
   };
 }
 
+// muestra los datos capturados en un formato específico.
 const FormPintar: React.FC<FormPintarProps> = ({ data }) => {
   return (
     <div>
